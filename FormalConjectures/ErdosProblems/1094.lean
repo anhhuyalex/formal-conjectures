f1ba24fc9a64468c094955e110f5f4c446e49444
anhhuyalex <alexn@minerva.kgi.edu>
2025-12-10 01:42:21 -0500

    problem 1094

---
+++ FormalConjectures/ErdosProblems/1094.lean
@@ -0,0 +1,34 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+
+import FormalConjectures.Util.ProblemImports
+import FormalConjectures.ForMathlib.Data.Nat.Full
+
+/-!
+# Erdős Problem 1094
+
+*References:*
+- [erdosproblems.com/1094](https://www.erdosproblems.com/1094)
+- [ELS88] Erdos, P. and Lacampagne, C. B. and Selfridge, J. L., Prime factors of binomial coefficients and related problems. Acta Arith. (1988), 507--523.
+-/
+
+
+namespace Erdos1094
+
+@[category research open, AMS 11]
+theorem erdos_1094 : True := sorry
+end Erdos1094
